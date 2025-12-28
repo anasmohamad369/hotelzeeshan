238ccb9177ecfcd68fd666872d3e8c1a87f051dc
ANAS-RYZER <anas@ryzer.app>
2025-12-28 11:31:31 +0530

    done

--- components/cart-drawer.tsx
+++ components/cart-drawer.tsx
@@ -40,6 +40,7 @@ export default function CartDrawer({ isOpen, onClose, cart, onAdd, onRemove, onC
             qty: item.quantity,
             price: item.price,
           })),
+          discount: discount,
           total: total,
         }),
       })
